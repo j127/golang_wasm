38026fd90ec31954e554473a87b1d67a4d4550ff
J. Cohen <hostelmarketing@gmail.com>
2020-08-01 02:36:27 -0700

    Update index.html, add JS function in Go

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -2,6 +2,38 @@ package main
 
 import "fmt"
 
+func prettyJson(input string) (string, error) {
+	var raw interface{}
+	if err := json.Unmarshall([]byte(input), &raw; err != nil {
+		return "", err
+	})
+
+	pretty, err := json.MarshallIndent(raw, "", "    ")
+	if err != nil {
+		return "", err
+	}
+
+	return string(pretty), nil
+}
+
+func jsonWrapper() js.Func {
+	jsonFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		if len(args) != 1 {
+			return "invalid number of arguments passed"
+		}
+
+		inputJSON := args[0].String()
+		fmt.Printf("input %s\n", inputJSON)
+		pretty, err := prettyJson(inputJSON)
+		if err != nil {
+			fmt.Printf("unable to convert to json %s\n", err)
+			return err.Error()
+		}
+		return pretty
+	})
+	return jsonFunc
+}
+
 func main() {
 	fmt.Println("hello from wasm")
 }
