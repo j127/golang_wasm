50e24c4e6416480730f1c73ff8fd94558c788e3e
J. Cohen <hostelmarketing@gmail.com>
2020-08-02 02:08:29 -0700

    Get the thing to run

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -11,21 +11,18 @@ func prettyJson(input string) (string, error) {
 	if err := json.Unmarshal([]byte(input), &raw); err != nil {
 		return "", err
 	}
-
 	pretty, err := json.MarshalIndent(raw, "", "    ")
 	if err != nil {
 		return "", err
 	}
-
 	return string(pretty), nil
 }
 
 func jsonWrapper() js.Func {
 	jsonFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		if len(args) != 1 {
-			return "invalid number of arguments passed"
+			return "Invalid no of arguments passed"
 		}
-
 		inputJSON := args[0].String()
 		fmt.Printf("input %s\n", inputJSON)
 		pretty, err := prettyJson(inputJSON)
@@ -39,5 +36,7 @@ func jsonWrapper() js.Func {
 }
 
 func main() {
-	fmt.Println("hello from wasm")
+	fmt.Println("Go Web Assembly")
+	js.Global().Set("formatJSON", jsonWrapper())
+	<- make(chan bool)
 }
